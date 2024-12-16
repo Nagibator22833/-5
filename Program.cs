ba0b5b7de30076605e32e2fb28d23aec083fa123
Emill <asasindark22@gmail.com>
2024-12-17 00:41:38 +0200

    creating an interface for, which has methods for implementing arithmetic operations

--- Program.cs
+++ Program.cs
@@ -2,7 +2,14 @@
 using System.Numerics;
 namespace Лаба5
 {
-    
+    public interface IMyNumber<T> where T : IMyNumber<T>
+    {
+        T Add(T b);
+        T Subtract(T b);
+        T Multiply(T b);
+        T Divide(T b);
+
+    }
     
     
     
