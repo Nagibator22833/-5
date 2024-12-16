f4f69dffc1976b3bdf98fd2bde941a0eab021032
Emill <asasindark22@gmail.com>
2024-12-17 00:35:25 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+namespace Лаба5
+{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
